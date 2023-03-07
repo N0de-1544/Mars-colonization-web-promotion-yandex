db25a8c93ece69428df5a4fea8e3750ddcb2cf3e
N0de-1544 <96366518+N0de-1544@users.noreply.github.com>
2023-03-07 18:49:31 +1000

    Add files via upload

--- website.py
+++ website.py
@@ -1,73 +1,10 @@
-from flask import Flask
-from flask import url_for
-from flask import request
-import requests
+from flask import Flask, url_for,request
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def start():
-    return "Миссия Колонизация Марса"
-
-
-@app.route('/index')
-def marscolonization():
-    return "И на Марсе будут яблони цвести!"
-
-@app.route('/image_mars')
-def mars():
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='img/mars.png')}" alt="Печаль, беда! Марса нет!">
-                    <h4>Вот она какая, красная планета. </h4>
-                  </body>
-                </html>'''
-
-
-
-@app.route('/promotion_image')
-def colonization_promotion():
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Колонизация</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='img/mars.png')}" alt="Печаль, беда! Марса нет!">
-                    <div class="alert alert-dark" role="alert">
-                      Человечество вырастает из детства.
-                    </div>
-                    <div class="alert alert-info" role="alert">
-                      Человечеству мала одна планета.
-                    </div>
-                    <div class="alert alert-secondary" role="alert">
-                      Мы сделаем обитаемыми безжизненные пока планеты.
-                    </div>
-                      <div class="alert alert-warning" role="alert">
-                      И начнем с Марса!
-                    </div>
-                      <div class="alert alert-danger" role="alert">
-                      Присоединяйся!
-                    </div>
-                  </body>
-                </html>'''
-
-
-@app.route('/astronaut_section', methods=['POST', 'GET'])
+@app.route('/form_sample', methods=['POST', 'GET'])
 def form_sample():
     if request.method == 'GET':
         return f'''<!doctype html>
@@ -86,25 +23,32 @@ def form_sample():
                             <h1>Форма для регистрации в суперсекретной системе</h1>
                             <div>
                                 <form class="login_form" method="post">
-                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
-                                    <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
+                                    <input type="text" class="form-control" id="name"  placeholder="Введите Имя" name="name">
+                                    <input type="text" class="form-control" id="surname" placeholder="Введите Фамилию" name="surname">
+                                    <input type="email" class="form-control" id="email" placeholder="Введите e-mail" name="email">
                                     <div class="form-group">
-                                        <label for="classSelect">В каком вы классе</label>
+                                        <label for="classSelect">Какое ваше образование?</label>
                                         <select class="form-control" id="classSelect" name="class">
-                                          <option>7</option>
-                                          <option>8</option>
-                                          <option>9</option>
-                                          <option>10</option>
-                                          <option>11</option>
+                                          <option>Дошкольное образование</option>
+                                          <option>Начальное общее образование</option>
+                                          <option>Основное общее образование</option>
+                                          <option>Среднее общее образование</option>
+                                          <option>Среднее профессиональное образование</option>
+                                          <option>Бакалавриат</option>
+                                          <option>Магистратура</option>
+                                          <option>Специалитет</option>
                                         </select>
-                                     </div>
-                                    <div class="form-group">
-                                        <label for="about">Немного о себе</label>
-                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    <div class="form-group form-check>
+                                        <label for="form-check">Какие у Вас есть профессии?</label><br>
+                                        <input type="checkbox" name="prof_1" value="engi-res"> Инженер-исследователь<br>
+                                        <input type="checkbox" name="prof_2" value="pilot"> Пилот<br>
+                                        <input type="checkbox" name="prof_3" value="builder"> Строитель<br>
+                                        <input type="checkbox" name="prof_4" value="exobio"> Экзобиолог<br>
+                                        <input type="checkbox" name="prof_5" value="radprot"> Специалист по радиационной защите<br>
+                                        <input type="checkbox" name="prof_6" value="doctor"> Врач<br>
+                                        <input type="checkbox" name="prof_7" value="cyberengi"> Киберинженер<br>
+                                        <br>       
                                     </div>
                                     <div class="form-group">
                                         <label for="form-check">Укажите пол</label>
@@ -121,9 +65,17 @@ def form_sample():
                                           </label>
                                         </div>
                                     </div>
+                                    <div class="form-group">
+                                        <label for="about">Почему Вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
                                     <div class="form-group form-check">
                                         <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                        <label class="form-check-label" for="acceptRules">Готов быть добровольцем</label>
+                                        <label class="form-check-label" for="acceptRules">Готов работать за идею.</label>
                                     </div>
                                     <button type="submit" class="btn btn-primary">Записаться</button>
                                 </form>
@@ -139,6 +91,10 @@ def form_sample():
         print(request.form['accept'])
         print(request.form['sex'])
         return "Форма отправлена"
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
